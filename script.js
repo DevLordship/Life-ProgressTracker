b0878a7571f46583c14fa7a9ce72eaa3864f0ae9
DevLordship <devlordship@gmail.com>
2023-10-27 13:00:48 +0530

    firebase connected

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+    getDatabase,
+    ref,
+    push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+    databaseURL:
+        "https://mytodoapp-6ebee-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const projectListInDB = ref(database, "projectList");
+
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
+const projectListEl = document.getElementById("project-list");
 
 addButtonEl.addEventListener("click", function () {
     let inputValue = inputFieldEl.value;
+    push(projectListInDB, inputValue);
+    clearInputFieldEl();
 
-    console.log(inputValue);
+    appendItemToprojectListEl(inputValue);
 });
+
+function clearInputFieldEl() {
+    inputFieldEl.value = "";
+}
+
+function appendItemToprojectListEl(itemValue) {
+    projectListEl.innerHTML += `<li>${itemValue}</li>`;
+}
